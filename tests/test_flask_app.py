ffc67e953f5587dc36ac236f076ec19f5bbe859d
Prashant Hule <prashant.hule6999@gmail.com>
2026-03-23 23:47:29 +0530

    cicd runs - 14

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,10 +1,26 @@
 import unittest
+import mlflow
+import os
 from flask_app.app import app
 
 class FlaskAppTests(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        # Set up DagsHub credentials for MLflow tracking
+        dagshub_token = os.getenv("CAPSTONE_TEST")
+        if not dagshub_token:
+            raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+        dagshub_url = "https://dagshub.com"
+        repo_owner = "prashanthule6999"
+        repo_name = "YT-Capstone-Project"
+
+        # Set up MLflow tracking URI
+        mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
         cls.client = app.test_client()
 
     def test_home_page(self):
