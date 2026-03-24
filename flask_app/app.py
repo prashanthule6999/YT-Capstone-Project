389e25aba01bf484e285a2bb95368ac608e21b15
Prashant Hule <prashant.hule6999@gmail.com>
2026-03-24 21:18:55 +0530

    cicd runs - 27

--- flask_app/app.py
+++ flask_app/app.py
@@ -184,10 +184,11 @@ def home():
     REQUEST_LATENCY.labels(endpoint="/").observe(time.time() - start_time)
     return response
 
-@app.before_serving
 def initialize():
     ensure_nltk_data()
     load_model()
+
+initialize()
 
 @app.route("/predict", methods=["POST"])
 def predict():
